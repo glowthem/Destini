4716a15f8f97c5ce8fe8c4fa4d445e55179a2cd3
glowthem <glowing713@naver.com>
2020-07-06 15:56:07 +0900

    2020-07-06 ➕ set title of text box, buttons

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,10 +5,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    let story0 = "You see a fork in the road"
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
     }
 
 
